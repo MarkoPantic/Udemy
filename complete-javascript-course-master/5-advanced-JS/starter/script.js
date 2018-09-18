b3f6bc68354b166a5e90f17968b27ea1a7203fb6
Marko <marko.pantic@klett.rs>
2018-09-18 16:11:11 +0200

    Posao

--- complete-javascript-course-master/5-advanced-JS/starter/script.js
+++ complete-javascript-course-master/5-advanced-JS/starter/script.js
@@ -54,35 +54,101 @@
 //  console.log(jane)
 
 // Primitives
-var a = 23;
-var b = a;
-a = 46;
+// var a = 23;
+// var b = a;
+// a = 46;
 
-//Objects
-var obj1 = {
-    name: 'John',
-    age: 26
-}
+// //Objects
+// var obj1 = {
+//     name: 'John',
+//     age: 26
+// }
 
-var obj2 = obj1;
+// var obj2 = obj1;
 
-obj1.age = 30;
+// obj1.age = 30;
+// //Functions
+
+
+
+// var age = 27;
+// var obj = {
+//     name: 'Jonas',
+//     city: 'Lisabon'
+// }
+
+// function change(a, b) {
+//     a = 30;
+//     b.city = 'Beograd'
+// }
+
+// change(age, obj);
+
+// console.log(age);
+// console.log(obj);
+
+
 //Functions
 
+// var years = [1990, 1965, 1937, 2005, 1998];
+
+// function arrayCalc(arr, fn) {
+//     var arrRes = [];
+//     for(var i = 0; i < arr.length; i++){
+//         arrRes.push(fn(arr[i]));
+//     }
+//     return arrRes;
+// }
+
+// function calculateAge(el){
+//     return 2018 - el;
+// }
+
+// function isFullAge(el) {
+//     return el >= 18;
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+
+// var fullAges = arrayCalc(ages, isFullAge);
+
+// console.log(fullAges);
 
 
-var age = 27;
-var obj = {
-    name: 'Jonas',
-    city: 'Lisabon'
+// function maxHR(el) {
+
+//     if (el >= 18 && el <= 81) {
+
+//     return Math.round(206.9 - (0.67 * el));;
+
+//     } else {
+//         return -1
+//     }
+
+// }
+
+// var hr = arrayCalc(ages, maxHR)
+
+// console.log(hr)
+
+//Functions returning functions
+
+function interviewQuestion(job) {
+    if(job === 'designer'){
+        return function(name){
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if(job === 'teacher'){
+        return function (name) {
+            console.log('What subject do you teach? ' + name);
+        }
+    } else {
+        return function (name) {
+            console.log('Hello ' + name + ' what do you do?');
+        }
+    }
 }
 
-function change(a, b) {
-    a = 30;
-    b.city = 'Beograd'
-}
+var teacherQuestion = interviewQuestion('teacher');
 
-change(age, obj);
-
-console.log(age);
-console.log(obj);
+teacherQuestion('Mark')
