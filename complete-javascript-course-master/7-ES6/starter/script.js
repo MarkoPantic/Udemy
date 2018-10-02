adeac207984d353d82a51c4757276d98ed1f4164
MarkoPantic <cockta90@gmail.com>
2018-10-02 06:36:08 +0200

    Jutro

--- complete-javascript-course-master/7-ES6/starter/script.js
+++ complete-javascript-course-master/7-ES6/starter/script.js
@@ -26,16 +26,66 @@
 // driversLicence(true)
 
 //ES6
-function driversLicence(passedTest){
-    if(passedTest){
-        let firstName = 'John';
-        const yearOfBirth = 1990;
-        console.log(firstName + ' born in ' + yearOfBirth);
-    }
+// function driversLicence(passedTest){
+//     if(passedTest){
+//         let firstName = 'John';
+//         const yearOfBirth = 1990;
+//         console.log(firstName + ' born in ' + yearOfBirth);
+//     }
 
     
-}
+// }
 
-driversLicence(true)
+// driversLicence(true)
+
+
+
+// {
+//     let a = 1;
+//     const b = 2;
+
+// }
+
+// console.log(a + b);
+
+
+
+// let firstName = 'john';
+
+// let lastName = 'Smith';
+
+// const yearOfBirth = 1990;
+
+// function calcAge(year) {
+//     return 2018 - year;
+// }
+
+
+// // console.log('this is ' + lastName)
+
+
+// console.log(`This is ${firstName} he was born in ${yearOfBirth} and today he is ${calcAge(yearOfBirth)}`);
+
+
+// const n = `${firstName} ${lastName}`
+
+// console.log(n.startsWith('j'))
+
+// const years = [1990, 1991, 1992, 1993];
+
+// let ages6 = years.map(el => 2016 - el);
+
+
+
+// ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`)
+
+
+// ages6 = years.map((el, index) => {
+//     const now = new Date().getFullYear();
+//     const age = now - el;
+//     return `Age element is ${index + 1}: ${age}`
+// })
+// console.log(ages6)
+
 
 
