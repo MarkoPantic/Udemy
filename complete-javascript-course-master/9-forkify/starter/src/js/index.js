759a222ab2fc8fded13e9deb9c37e4df6fe65d6b
MarkoPantic <cockta90@gmail.com>
2018-10-12 18:54:05 +0200

    Kuca

--- complete-javascript-course-master/9-forkify/starter/src/js/index.js
+++ complete-javascript-course-master/9-forkify/starter/src/js/index.js
@@ -1,5 +1,45 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+/*Global state of the app
 
-const search = new Search('pizza');
+- Search object
+- Current recipe object
+- Shopping list object
+- Liked recipes
 
-search.getResults();
+*/
+
+
+const state = {};
+
+const controlSearch = async () => {
+    // - get query from the view
+    const query = searchView.getInput(); // for now!
+
+    if(query){
+        // - Create new search obj and add to state obj
+        state.search = new Search(query);
+
+        // - prepare UI for results
+
+        searchView.clearInput();
+
+        // - clear results from the previus
+
+        searchView.clearResults();
+
+        // - search for recipes
+        await state.search.getResults();
+
+        // render results on UI
+
+        searchView.renderResults(state.search.result)
+    }
+}
+
+
+elements.searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    controlSearch();
+})
