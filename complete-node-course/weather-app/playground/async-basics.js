acb6d34cb9bdd5f7f9d02db228f5db967823687b
Marko <marko.pantic@klett.rs>
2018-11-26 16:20:34 +0100

    weather-app

---
+++ complete-node-course/weather-app/playground/async-basics.js
@@ -0,0 +1,16 @@
+console.log(`Starting app.`);
+
+setTimeout(() => {
+   
+    console.log(`Timeout`);
+
+}, 2000);
+
+
+setTimeout(() => {
+
+    console.log(`2nd`);
+
+}, 0);
+
+console.log(`Finishing up!`);
