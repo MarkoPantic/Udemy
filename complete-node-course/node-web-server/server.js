18f565541e5c0a92a6d308afbca05d024da42e51
MarkoPantic <cockta90@gmail.com>
2018-12-05 22:31:32 +0100

    setup start script and heroku port

--- complete-node-course/node-web-server/server.js
+++ complete-node-course/node-web-server/server.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+const maintenance = false;
+const port = process.env.PORT || 8000;
 
 var app = express();
 
+
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+})
+
+app.use((req, res, next) => {
+    if (maintenance) {
+
+        res.render('maintenance.hbs', {
+            title: 'We will be right back',
+            body: 'See you soon!! pozz'
+        })
+    } else {
+        next();
+    }
+})
+
 app.use(express.static(__dirname + '/public'));
-
-
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 
 hbs.registerHelper('screamIt', (text) => text.toUpperCase())
@@ -34,6 +60,6 @@ app.get('/bad', (req, res) => {
     })
 })
 
-app.listen(8000, () => {
-    console.log('Server is up on port 8000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
